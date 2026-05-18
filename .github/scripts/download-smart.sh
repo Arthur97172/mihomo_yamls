41fc99b7491c2434447add9a5a11b79e2d778aec
Chiao Yenta <68536499+HenryChiao@users.noreply.github.com>
2026-05-18 15:54:15 +0800

    Update YAML file URLs in download-smart.sh

--- .github/scripts/download-smart.sh
+++ .github/scripts/download-smart.sh
@@ -9,13 +9,13 @@ https://raw.githubusercontent.com/666OS/YYDS/main/mihomo/config/OneSmart.yaml|TH
 https://raw.githubusercontent.com/HenryChiao/MIHOMO_AIO/refs/heads/main/CONFIG/SMART/MihomoSmartProPlus.yaml|THEYAMLS/Smart_Mode/HenryChiao/MihomoSmartProPlus.yaml
 https://raw.githubusercontent.com/HenryChiao/MIHOMO_AIO/refs/heads/main/CONFIG/SMART/MihomoSmartAIO.yaml|THEYAMLS/Smart_Mode/HenryChiao/MihomoSmartAIO.yaml
 https://raw.githubusercontent.com/HenryChiao/MIHOMO_AIO/refs/heads/main/CONFIG/SMART/MihomoSmartProMax.yaml|THEYAMLS/Smart_Mode/HenryChiao/MihomoSmartProMax.yaml
-https://raw.githubusercontent.com/HenryChiao/MIHOMO_AIO/refs/heads/main/CONFIG/Test%20version/the_smart_test.yaml|THEYAMLS/Smart_Mode/HenryChiao/THESmart.yaml
+https://raw.githubusercontent.com/HenryChiao/MIHOMO_AIO/refs/heads/main/CONFIG/Test_Final/mihomo-final-v6.yaml|THEYAMLS/Smart_Mode/HenryChiao/THESmart.yaml
 https://raw.githubusercontent.com/EdisonLe12/MIHOMO_YAMLS/refs/heads/main/OneSmartProMCX.yaml|THEYAMLS/Smart_Mode/edison/OneSmartProMCX.yaml
 https://raw.githubusercontent.com/liandu2024/little/refs/heads/main/yaml/clash-fallback-smart-std.yaml|THEYAMLS/Smart_Mode/liandu2024/clash-fallback-smart-std.yaml
 https://raw.githubusercontent.com/liandu2024/little/refs/heads/main/yaml/clash-all-smart.yaml|THEYAMLS/Smart_Mode/liandu2024/clash-all-smart.yaml
 https://raw.githubusercontent.com/liandu2024/little/refs/heads/main/yaml/clash-all-fallback-smart.yaml|THEYAMLS/Smart_Mode/liandu2024/clash-all-fallback-smart.yaml
 https://raw.githubusercontent.com/echs-top/proxy/heads/main/mihomo_smart.yaml|THEYAMLS/Smart_Mode/echs-top/mihomo_smart.yaml
-https://raw.githubusercontent.com/qichiyuhub/rule/refs/heads/main/config/mihomo/AI/smart.yaml|THEYAMLS/Smart_Mode/qichiyuhub/smart.yaml
+https://raw.githubusercontent.com/qichiyuhub/rule/refs/heads/main/config/mihomo/smart/smart.yaml|THEYAMLS/Smart_Mode/qichiyuhub/smart.yaml
 EOF
 )
 
